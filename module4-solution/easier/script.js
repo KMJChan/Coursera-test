62e9c9a1f4c10b3cff84759a9c3b438fd15fb65e
Janet Chan <janettchan@gmail.com>
2018-03-31 16:01:14 -0400

    Revised Assignment4.

--- module4-solution/easier/script.js
+++ module4-solution/easier/script.js
@@ -45,6 +45,12 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
+  console.log("Hello" + names[i]);
+}
+
+for (var i = 0; i < names.length; i++) {
+  console.log("Good Bye " + names[i]);
+}
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
